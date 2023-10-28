b25cf5834d0e772b55640a126f877dfbf2f338c7
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-10-28 20:06:14 +0100

    third commit - tp video part 1

--- gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package org.sid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +16,17 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Bean
+	/*//@Bean
 	RouteLocator routeLocator(RouteLocatorBuilder builder){
 		return builder.routes()
-				.route("r1", r -> r.path("/customers/**").uri("http://localhost:8081/"))
-				.route("r2", r -> r.path("/products/**").uri("http://localhost:8082/"))
+				.route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+				.route("r2", r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
 				.build();
+	}*/
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties properties){
+		return  new DiscoveryClientRouteDefinitionLocator(rdc, properties);
 	}
 
 
